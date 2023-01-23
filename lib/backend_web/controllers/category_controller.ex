badaa11cc855cfbecfed3aabf33af51c04a13bec
Lauro Nicolas Mazotti <lauro.nicolas.mazotti@gmail.com>
2023-01-23 11:30:34 -0300

    Usando fallback controller para mostrar erros

--- lib/backend_web/controllers/category_controller.ex
+++ lib/backend_web/controllers/category_controller.ex
@@ -1,6 +1,10 @@
 defmodule BackendWeb.CategoryController do
   use BackendWeb, :controller
   alias Backend.Categories
+  alias Backend.Categories.Category
+
+  action_fallback BackendWeb.FallbackController
+
   def index(conn, _params) do
     conn
     |> put_status(:ok)
@@ -12,10 +16,11 @@ defmodule BackendWeb.CategoryController do
   end
 
   def create(conn, %{"category" => category}) do
-    {:ok, category} = Categories.create(category)
-    conn
-    |> put_status(:created)
-    |> put_resp_header("location", Routes.category_path(conn, :show, category))
-    |> render("show.json", category: category)
+    with {:ok, %Category{} = category} <- Categories.create(category) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.category_path(conn, :show, category))
+      |> render("show.json", category: category)
+    end
   end
 end
